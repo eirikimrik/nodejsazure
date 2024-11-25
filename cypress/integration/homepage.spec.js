e21feb4cc5f0bd6718ac0b01958584b79e97dd3c
Eirik <eirik.imrik@gmail.com>
2024-11-25 13:22:51 +0100

    configured cypress

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,6 +1,6 @@
 describe("Homepage Tests", () => {
   it("should display the welcome message", () => {
-    cy.visit("https://eirik-app.azurewebsites.net"); 
+    cy.visit("https://eirik-app.azurewebsites.net"); // Replace with your app URL
     cy.contains("Welcome to Azure");
   });
 });
