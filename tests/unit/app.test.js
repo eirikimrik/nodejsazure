65d063b529947bcc5d5e716450b0693c0f773a2a
Eirik <eirik.imrik@gmail.com>
2024-11-26 23:17:48 +0100

    updating test

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("should return a welcome message", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain("Welcome to Azure");
+    expect(res.text).toContain("Eirik's first containerized web app!");
   });
 });
