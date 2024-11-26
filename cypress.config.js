24ccbb2756f79d27130c2850ab25e351508ada7b
Eirik <eirik.imrik@gmail.com>
2024-11-26 15:04:54 +0100

    Modified cypress confoig file after failed test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://eirik-app.azurewebsites.net",
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // Implement node event listeners here if needed
     },
--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,15 +1,14 @@
-describe('Homepage', () => {
-    it('should load the homepage and display the title', () => {
-      cy.visit('https://eirik-app.azurewebsites.net'); 
-      cy.contains('Express');
-    });
+describe("Homepage", () => {
+  it("should load the homepage and display the title", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Express");
   });
-  
-  describe('Users Page', () => {
-    it('should load the users page and display users', () => {
-      cy.visit('https://eirik-app.azurewebsites.net/users'); 
-      cy.contains('John Doe'); 
-      cy.contains('Jane Doe');
-    });
+});
+
+describe("Users Page", () => {
+  it("should load the users page and display users", () => {
+    cy.visit("http://localhost:3000/users");
+    cy.contains("John Doe");
+    cy.contains("Jane Doe");
   });
-  
+});
